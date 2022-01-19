8699de21a1f76ad464b620156af8569e614732d5
jodelajo <jodelajo@gmail.com>
2022-01-19 14:51:49 +0100

    trying to style

--- src/components/Agencies.js
+++ src/components/Agencies.js
@@ -5,7 +5,7 @@ import "./Agencies.css";
 
 export default function Agencies({ agency }) {
   return (
-    <div>
+    <div className="main-agencies">
       <div className="agency-card-container">
         <div className="company-logo">
           <img
