eabd3cb73041ec24f94d55c1155de9713ca4a997
DariaKryv <krivosheevadarya@gmail.com>
2022-01-27 11:57:34 +0100

    Filter location on server side

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,44 +1,44 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Oval } from "react-loader-spinner";
 import axios from "axios";
 import "../Pages/HomePage.css";
 import Agencies from "../components/Agencies/Agencies";
 import LocationSearch from "../components/LocationSearch/LocationSearch";
+import { isEndOfPage } from "../utils/dataTransformations";
 
 export default function HomePage() {
   const [agencies, setAgencies] = useState([]);
   const page = useRef(1);
   const [searchInput, setSearchInput] = useState("");
 
-  console.log("AGENCIES", agencies);
-  console.log("searchInput in component", searchInput);
+  const fetchData = useCallback(async () => {
+    const pageToFetch = page.current;
+    page.current = page.current + 1;
+    const response = await axios.get(
+      `http://localhost:8000/agencies?_limit=15&_page=${pageToFetch}&city_like=${searchInput}`
+    );
 
-  const fetchData = async () => {
-    console.log("searchInput", searchInput);
-    // const response = await axios.get(
-    //   `http://localhost:8000/agencies?_limit=15&_page=${page.current}&city_like=${searchInput}`
-    // );
-    // console.log("GETMY", response);
+    const dataOfAgencies = response.data;
+    setAgencies((oldData) => [...oldData, ...dataOfAgencies]);
+  }, [searchInput]);
 
-    // const dataOfAgencies = response.data;
-    // setAgencies((oldData) => [...oldData, ...dataOfAgencies]);
-    // page.current = page.current + 1;
-  };
-
-  const handleScroll = (e) => {
-    if (
-      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
-      e.target.documentElement.scrollHeight
-    ) {
-      fetchData();
-    }
-  };
+  const handleScroll = useCallback(
+    (e) => {
+      if (isEndOfPage(e)) {
+        fetchData();
+      }
+    },
+    [fetchData]
+  );
 
   useEffect(() => {
+    page.current = 1;
+    setAgencies([]);
     fetchData();
     window.addEventListener("scroll", handleScroll);
-  }, [searchInput]);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [fetchData, handleScroll]);
 
   return (
     <div className="general">
