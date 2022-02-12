05f9ea79b35849d5fc5c58f9e403734424aa33c9
jodelajo <jodelajo@gmail.com>
2022-02-12 14:42:28 +0100

    install lodash.debounce

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -37,7 +37,7 @@ export default function HomePage() {
     setState((prevState) => {
       return {
         ...prevState,
-        agencies: [...prevState.agencies, ...response.data],
+        agencies: [...response.data],
         isLoading: false,
       };
     });
